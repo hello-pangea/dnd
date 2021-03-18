23f02d402887dbb2f50f677c323589d71b4d9554
Daniel Del Core <daniel.delcore.92@gmail.com>
2021-03-18 14:42:52 +1100

    Merge pull request #1838 from vladmoroz/export-default-sensors
    Export default sensors

--- src/view/use-sensor-marshal/index.ts
+++ src/view/use-sensor-marshal/index.ts
@@ -1,1 +1,4 @@
 export { default } from './use-sensor-marshal';
+export { default as useMouseSensor } from './sensors/use-mouse-sensor';
+export { default as useTouchSensor } from './sensors/use-touch-sensor';
+export { default as useKeyboardSensor } from './sensors/use-keyboard-sensor';
