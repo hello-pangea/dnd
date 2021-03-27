7079373bcf2ce6c11c34b9d0b814384c83324079
Gabriel Santerre <contact@gabrielsanterre.com>
2021-03-27 13:11:46 -0400

    Fix NodeJS.Timer issue

--- src/global.d.ts
+++ src/global.d.ts
@@ -15,3 +15,13 @@ declare const process: Process;
 declare interface Window {
   '__@react-forked/dnd-disable-dev-warnings'?: boolean;
 }
+
+// From the test folder we explicitly set typescript to load node.
+// From the stories folder @storybook/react load NodeJS types.
+// See: https://github.com/storybookjs/storybook/issues/12969#issue-733867804
+// Loading node can causes issues similar to what is mention below
+// See: https://github.com/Microsoft/TypeScript/issues/842
+// In order to fix the problem we override some node types
+declare namespace NodeJS {
+  declare type Timeout = TimeoutID;
+}
