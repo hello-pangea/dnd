e6642190da33f901d536c1fad8062e1959a4fa97
Gabriel Santerre <gab@100terres.com>
2022-02-05 00:41:26 -0500

    Fix stylint types

--- test/unit/view/style-marshal/get-styles.spec.ts
+++ test/unit/view/style-marshal/get-styles.spec.ts
@@ -5,7 +5,7 @@ import type { Styles } from '../../../../src/view/use-style-marshal/get-styles';
 
 const styles: Styles = getStyles('hey');
 
-Object.keys(styles).forEach((key: string) => {
+(Object.keys(styles) as Array<keyof Styles>).forEach((key) => {
   it(`should generate valid ${key} styles`, () =>
     stylelint
       .lint({
@@ -23,7 +23,9 @@ Object.keys(styles).forEach((key: string) => {
       .then((result) => {
         expect(result.errored).toBe(false);
         // asserting that some CSS was actually generated!
-        // eslint-disable-next-line no-underscore-dangle
-        expect(result.results[0]._postcssResult.css.length).toBeGreaterThan(1);
+        expect(
+          // eslint-disable-next-line no-underscore-dangle
+          (result.results[0] as any)._postcssResult.css.length,
+        ).toBeGreaterThan(1);
       }));
 });
