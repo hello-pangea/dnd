cd368abdfc2f4e22e8ac8def48f6b7136105bc33
Gabriel Santerre <gab@100terres.com>
2022-02-06 13:56:14 -0500

    Fix console util types

--- test/util/console.ts
+++ test/util/console.ts
@@ -1,6 +1,6 @@
 import { noop } from '../../src/empty';
 
-function withConsole(type: string, fn: () => void, message?: string) {
+function withConsole(type: 'error' | 'warn', fn: () => void, message?: string) {
   const mock = jest.spyOn(console, type).mockImplementation(noop);
 
   fn();
@@ -17,7 +17,7 @@ function withConsole(type: string, fn: () => void, message?: string) {
 export const withError = withConsole.bind(null, 'error');
 export const withWarn = withConsole.bind(null, 'warn');
 
-function withoutConsole(type: string, fn: () => void) {
+function withoutConsole(type: 'error' | 'warn', fn: () => void) {
   const mock = jest.spyOn(console, type).mockImplementation(noop);
 
   fn();
@@ -29,7 +29,7 @@ function withoutConsole(type: string, fn: () => void) {
 export const withoutError = withoutConsole.bind(null, 'error');
 export const withoutWarn = withoutConsole.bind(null, 'warn');
 
-function disableConsole(type: string) {
+function disableConsole(type: 'error' | 'warn') {
   let mock: jest.SpyInstance;
 
   beforeEach(() => {
