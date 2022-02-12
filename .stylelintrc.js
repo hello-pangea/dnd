c8b899b66bfd86400a8be847feda19aa661cdcb5
Gabriel Santerre <gab@100terres.com>
2022-02-12 00:09:53 -0500

    Disable function-no-unknown rule

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -13,6 +13,11 @@ module.exports = {
     "stylelint-config-prettier"
   ],
   "rules": {
+    // Temporaly disabled, because of an issue with this rule
+    // See: - https://github.com/stylelint/stylelint/issues/5904
+    //      - https://github.com/niksy/css-functions-list/issues/2
+    "function-no-unknown": null,
+
     "declaration-empty-line-before": null,
     "comment-empty-line-before": null,
     "block-no-empty": null,
