5ec5a7467882e66bd707bbb4fb7426b70f462ac5
Gabriel Santerre <gab@100terres.com>
2022-04-13 22:30:43 -0400

    test(jsdom env): fix issue with missing TransitionEvent

--- test/setup/environment.ts
+++ test/setup/environment.ts
@@ -4,6 +4,7 @@ import JSDOMEnvironment from 'jest-environment-jsdom';
 import { TextDecoder, TextEncoder } from 'util';
 
 import attachRafStub from './attach-raf-stub';
+import transitionEventPolyfill from './transition-event-polyfill';
 
 declare global {
   interface ProcessEnv {
@@ -16,6 +17,7 @@ export default class MyJSDOMEnvironment extends JSDOMEnvironment {
     super(config, context);
 
     attachRafStub.call(this);
+    transitionEventPolyfill.call(this);
 
     // When importing jsdom in one of the test it throws an
     // error, because TextDecoder and TextEncoder are needed.
