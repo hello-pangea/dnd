5ed8406d602a9c694d67209a4a89caa45a7842fb
Ramon <r.splithof@exonet.nl>
2022-04-21 09:51:34 +0000

    fix: the pure property has been removed, `connect` is now alway a "pure/memoized" component

--- src/view/draggable/connected-draggable.ts
+++ src/view/draggable/connected-draggable.ts
@@ -380,10 +380,7 @@ const ConnectedDraggable = connect(
   {
     // Using our own context for the store to avoid clashing with consumers
     context: StoreContext as any,
-    // Default value, but being really clear
-    pure: true,
     // When pure, compares the result of mapStateToProps to its previous value.
-    // Default value: shallowEqual
     // Switching to a strictEqual as we return a memoized object on changes
     areStatePropsEqual: isStrictEqual,
   },
