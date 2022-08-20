2e186c3d7cbe8877480eb36fc19323acf9951dff
Gabriel Santerre <gab@100terres.com>
2022-08-20 13:04:30 -0400

    chore(type): fix react-virtualized type (library does not support react 18) (#392)

--- stories/src/virtual/react-virtualized/list.tsx
+++ stories/src/virtual/react-virtualized/list.tsx
@@ -91,7 +91,7 @@ function App(props: Props): ReactElement {
             rowCount={quotes.length}
             rowHeight={110}
             width={300}
-            ref={(ref) => {
+            ref={(ref: any) => {
               // react-virtualized has no way to get the list's ref that I can so
               // So we use the `ReactDOM.findDOMNode(ref)` escape hatch to get the ref
               if (ref) {
