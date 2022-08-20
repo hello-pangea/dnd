48bf66825e7382d79dd676bc851e1ada7378d633
Gabriel Santerre <gab@100terres.com>
2022-08-20 04:39:45 -0400

    chore(storybook): enable strict mode

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,16 @@
 require('dotenv').config();
 
 module.exports = {
-  addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-storysource',
-    'storybook-addon-performance/register',
-  ],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-storysource'],
   check: true,
   checkOptions: {
     tsconfig: '../stories/tsconfig.json',
   },
   core: {
     disableTelemetry: true,
+  },
+  reactOptions: {
+    strictMode: true,
   },
   stories: [
     '../stories/**/*.stories.mdx',
