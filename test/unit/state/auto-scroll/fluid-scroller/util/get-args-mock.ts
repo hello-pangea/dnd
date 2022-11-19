8b21cbbe27ec05fed0304069fe6535c7f93b9b57
Reece Carolan <reece@mycopilot.com>
2022-11-18 17:46:23 -0800

    fix: broken import in test

--- test/unit/state/auto-scroll/fluid-scroller/util/get-args-mock.ts
+++ test/unit/state/auto-scroll/fluid-scroller/util/get-args-mock.ts
@@ -1,14 +1,14 @@
 import type { Position } from 'css-box-model';
-import { AutoScrollConfig } from '../../../../../../src/state/auto-scroller/fluid-scroller/config/autoscroll-config-types';
+import { AutoScrollOptions } from '../../../../../../src/state/auto-scroller/fluid-scroller/autoscroll-config-types';
 import type { DroppableId } from '../../../../../../src/types';
 
-export default (autoScrollConfig?: AutoScrollConfig) => {
+export default (autoScrollOptions?: AutoScrollOptions) => {
   const scrollWindow = jest.fn<void, [Position]>();
   const scrollDroppable = jest.fn<void, [DroppableId, Position]>();
 
   return {
     scrollWindow,
     scrollDroppable,
-    autoScrollOptions: autoScrollConfig,
+    autoScrollOptions,
   };
 };
