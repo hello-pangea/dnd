dc6e282fcc50edca195a27f9458fbc085e5a1782
spectraldoy <littleguyisfum@gmail.com>
2022-11-21 23:00:51 -0800

    fix: eslint errors in unit test after refactor to autoScrollerOptions

--- test/unit/state/auto-scroll/fluid-scroller/util/get-args-mock.ts
+++ test/unit/state/auto-scroll/fluid-scroller/util/get-args-mock.ts
@@ -1,8 +1,8 @@
 import type { Position } from 'css-box-model';
-import { AutoScrollOptions } from '../../../../../../src/state/auto-scroller/fluid-scroller/auto-scroller-options-types';
+import { AutoScrollerOptions } from '../../../../../../src/state/auto-scroller/fluid-scroller/auto-scroller-options-types';
 import type { DroppableId } from '../../../../../../src/types';
 
-export default (autoScrollerOptions?: AutoScrollOptions) => {
+export default (autoScrollerOptions?: AutoScrollerOptions) => {
   const scrollWindow = jest.fn<void, [Position]>();
   const scrollDroppable = jest.fn<void, [DroppableId, Position]>();
 
