1c9e218c47fcae9ef9d097904040c1f847a6f885
Reece Carolan <reece@mycopilot.com>
2023-06-20 11:39:41 -0700

    chore: typescript 5.1

--- .storybook/storybook-theme.ts
+++ .storybook/storybook-theme.ts
@@ -1,21 +1,22 @@
 import { create } from '@storybook/theming/create';
-import logo from './compressed-logo-rfd.svg';
 
-const brandTitle = `
-  <span
-    style="display:flex;align-items:center"
-    title="@hello-pangea/dnd"
-  >
-    <img
-      alt="Logo of @hello-pangea/dnd"
-      src="${logo}"
-      style="display:block;width:auto;height:42px;max-width:100%"
-    />
-    <span style="margin-left:8px;color:#0BAF7C;font-family:monospace;font-size:26px">
-      rfd
-    </span>
-  </span>
-`;
+// Old brand title
+// Broke in Storybook 7, will go without it for now
+// const brandTitle = `
+//   <span
+//     style="display:flex;align-items:center"
+//     title="@hello-pangea/dnd"
+//   >
+//     <img
+//       alt="Logo of @hello-pangea/dnd"
+//       src="${logo}"
+//       style="display:block;width:auto;height:42px;max-width:100%"
+//     />
+//     <span style="margin-left:8px;color:#0BAF7C;font-family:monospace;font-size:26px">
+//       rfd
+//     </span>
+//   </span>
+// `;
 
 export default create({
   base: 'light',
