1fcde776fbb5e7f38a189fbe7fef2cfa15524a06
Gabriel Santerre <gab@100terres.com>
2024-03-23 12:43:04 -0400

    chore(dev-deps): bump @babel/* and drop proposals

--- babel.config.js
+++ babel.config.js
@@ -26,9 +26,9 @@ module.exports = (api) => {
         },
       ],
       '@babel/plugin-transform-object-assign',
-      ['@babel/plugin-proposal-class-properties', { loose: true }],
-      ['@babel/plugin-proposal-private-methods', { loose: true }],
-      ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
       // used for stripping out the `invariant` messages in production builds
       isProduction ? 'babel-plugin-dev-expression' : false,
     ].filter(Boolean),
