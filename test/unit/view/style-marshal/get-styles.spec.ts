b0e79ec739f402aab171cd53265fde08464e9f14
Gabriel Santerre <gab@100terres.com>
2024-03-23 16:14:01 -0400

    chore(dev-deps): bump stylelint*

--- test/unit/view/style-marshal/get-styles.spec.ts
+++ test/unit/view/style-marshal/get-styles.spec.ts
@@ -14,29 +14,22 @@ import type { Styles } from '../../../../src/view/use-style-marshal/get-styles';
 const styles: Styles = getStyles('hey');
 
 (Object.keys(styles) as Array<keyof Styles>).forEach((key) => {
-  it(`should generate valid ${key} styles`, () =>
-    stylelint
-      .lint({
-        code: styles[key],
-        config: {
-          // just using the recommended config as it only checks for errors and not formatting
-          extends: ['stylelint-config-recommended'],
-          // basic semi colin rules
-          rules: {
-            'no-extra-semicolons': true,
-            'declaration-block-semicolon-space-after': 'always-single-line',
-          },
-        },
-      })
-      .then((result) => {
-        expect(result.errored).toBe(false);
+  it(`should generate valid ${key} styles`, async () => {
+    const result = await stylelint.lint({
+      code: styles[key],
+      config: {
+        // just using the recommended config as it only checks for errors and not formatting
+        extends: ['stylelint-config-recommended'],
+      },
+    });
 
-        // asserting that some CSS was actually generated!
-        expect(
-          // Types on _postcssResult are not properly working,
-          // because the `css` is not avalaible
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any, no-underscore-dangle
-          (result.results[0]._postcssResult as any)?.css.length,
-        ).toBeGreaterThan(1);
-      }));
+    expect(result.errored).toBe(false);
+    // asserting that some CSS was actually generated!
+    expect(
+      // Types on _postcssResult are not properly working,
+      // because the `css` is not avalaible
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any, no-underscore-dangle
+      (result.results[0]._postcssResult as any)?.css.length,
+    ).toBeGreaterThan(1);
+  });
 });
