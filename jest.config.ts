11fe50e6b66378b5fa45d1fd82cf01888fbd9479
Gabriel Santerre <gab@100terres.com>
2025-01-22 14:56:53 -0500

    feat: add support for react v19
    This commit adds support for react v19 and ensures that the unit tests run on both react v18 and
    v19.

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,10 @@
 /// <reference path="./test/typings/environment.d.ts" />
 
 import type { Config } from 'jest';
+import getReactMajorVersion from './test/util/get-react-major-version';
 import isRunningInCI from './test/util/is-running-in-ci';
+
+const reactMajorVersion = getReactMajorVersion();
 
 const config: Config = {
   clearMocks: true,
@@ -25,6 +28,17 @@ const config: Config = {
     'jest-watch-typeahead/testname',
   ],
 };
+
+// eslint-disable-next-line no-console
+console.log('Testing with React version:', `${reactMajorVersion}.x.x`);
+
+if (reactMajorVersion === '18') {
+  config.cacheDirectory = `.cache/jest-cache-react-${reactMajorVersion}`;
+  config.moduleNameMapper = {
+    '^react-dom((\\/.*)?)$': `react-dom-${reactMajorVersion}$1`,
+    '^react((\\/.*)?)$': `react-${reactMajorVersion}$1`,
+  };
+}
 
 if (isRunningInCI()) {
   config.maxWorkers = 2;
--- test/typings/environment.d.ts
+++ test/typings/environment.d.ts
@@ -1,6 +1,7 @@
 interface ProcessEnv {
-  NODE_ENV?: 'development' | 'production';
   CI?: boolean;
+  NODE_ENV?: 'development' | 'production';
+  REACT_MAJOR_VERSION?: '18' | '19';
 }
 
 interface Process {
